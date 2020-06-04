76258a90c42a74a6e183bd067e808592d3a5107c
Mona Lisa <ssbyrne89@gmail.com>
2020-06-04 00:46:41 -0600

    rendered html and json

---
+++ web_app/routes/home_routes.py
@@ -0,0 +1,16 @@
+# web_app/routes/home_routes.py
+
+from flask import Blueprint
+
+home_routes = Blueprint("home_routes", __name__)
+
+@home_routes.route("/")
+def index():
+    print("Visiting the home page")
+    x = 2 + 2
+    return f"Hello World! {x}"
+
+@home_routes.route("/about")
+def about():
+    print("Visiting the home page")
+    return "About me"
